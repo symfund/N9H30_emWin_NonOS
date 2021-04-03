d1e8e3df04b7b8ad77f6f66fecb21defb3f511d2
Arthur <symfund@gmail.com>
2021-04-04 00:19:43 +0800

    make tool provided by msys64 and GNU ARM Eclipse Build Tool
    make tool can be provided by msys64 or GNU ARM Eclipse build tool

--- NuDemo/BuildCmd.bat
+++ NuDemo/BuildCmd.bat
@@ -1,6 +1,8 @@
 
 REM make tool path
-set MAKE_PATH=C:\msys64\usr\bin
+set make_tool_provided_by=C:\Program Files (x86)\GNU ARM Eclipse\Build Tools\2.8-201611221915\bin
+REM set make_tool_provided_by=C:\msys64\usr\bin
+set MAKE_PATH=%make_tool_provided_by%
 
 REM gcc tool path
 set GNU_TOOLS_ARM_EMBEDDED_PATH=C:\Program Files (x86)\GNU Tools ARM Embedded\6 2017-q1-update\bin
